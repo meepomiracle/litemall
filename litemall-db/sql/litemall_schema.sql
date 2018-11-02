c164188e80d39316444591ebbc6ccd07849db0e1
53315557@qq.com <53315557@qq.com>
2018-11-02 18:27:33 +0800

    fix config

--- litemall-db/sql/litemall_schema.sql
+++ litemall-db/sql/litemall_schema.sql
@@ -2,6 +2,6 @@ drop database if exists litemall;
 drop user if exists 'litemall'@'localhost';
 create database litemall;
 use litemall;
-create user 'litemall'@'localhost' identified by 'litemall123456';
+create user 'litemall'@'localhost' identified by 'litemall';
 grant all privileges on litemall.* to 'litemall'@'localhost';
 flush privileges;
